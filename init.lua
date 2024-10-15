9638a4c6d35da1410f20387cab2d239ea8c1aa4e
Alejandro Tomasella <aletomasella09@gmail.com>
2024-10-14 23:18:03 -0300

    feat: Adding Go LSP

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-
 -- Set <space> as the leader key
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
@@ -514,7 +513,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
